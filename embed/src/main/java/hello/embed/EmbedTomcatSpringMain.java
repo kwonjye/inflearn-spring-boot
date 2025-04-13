a4bec1abd06fbf0f97332dc487c55a86130dc642
KWONJYE <sheisdanmi@gmail.com>
2025-04-13 11:54:41 +0900

    [4/스프링 부트와 내장 톰캣] 내장 톰캣5 - 빌드와 배포2

--- embed/src/main/java/hello/embed/EmbedTomcatSpringMain.java
+++ embed/src/main/java/hello/embed/EmbedTomcatSpringMain.java
@@ -8,6 +8,8 @@ import org.apache.catalina.startup.Tomcat;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import java.io.File;
+
 public class EmbedTomcatSpringMain {
 
     public static void main(String[] args) throws LifecycleException {
@@ -28,6 +30,12 @@ public class EmbedTomcatSpringMain {
 
         //디스패처 서블릿 등록
         Context context = tomcat.addContext("", "/");
+        File docBaseFile = new File(context.getDocBase());
+        if (!docBaseFile.isAbsolute()) {
+            docBaseFile = new File(((org.apache.catalina.Host)
+                    context.getParent()).getAppBaseFile(), docBaseFile.getPath());
+        }
+        docBaseFile.mkdirs();
         tomcat.addServlet("", "dispatcher", dispatcher);
         context.addServletMappingDecoded("/", "dispatcher");
         tomcat.start();
