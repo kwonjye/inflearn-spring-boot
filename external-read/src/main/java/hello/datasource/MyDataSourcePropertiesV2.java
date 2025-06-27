c34808a1520fb2ea29dcbe58213d581eb5ad4fcf
KWONJYE <sheisdanmi@gmail.com>
2025-06-27 19:59:31 +0900

    [8/외부설정과 프로필2] 외부설정 사용 - @ConfigurationProperties 생성자

---
+++ external-read/src/main/java/hello/datasource/MyDataSourcePropertiesV2.java
@@ -0,0 +1,33 @@
+package hello.datasource;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+import java.time.Duration;
+import java.util.List;
+
+@Getter
+@ConfigurationProperties("my.datasource")
+public class MyDataSourcePropertiesV2 {
+    private String url;
+    private String username;
+    private String password;
+    private Etc etc;
+
+    public MyDataSourcePropertiesV2(String url, String username, String password, @DefaultValue Etc etc) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+        this.etc = etc;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Etc {
+        private int maxConnection;
+        private Duration timeout;
+        private List<String> options;
+    }
+}
